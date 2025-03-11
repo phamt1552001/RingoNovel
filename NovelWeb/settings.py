3883e91f88c2072a839ae9ceaf106c0772f77ef6
phamt1552001 <phamt1552001@gmail.com>
2025-03-11 18:11:57 +0700

    Cập nhật database trong file setting.py

--- NovelWeb/settings.py
+++ NovelWeb/settings.py
@@ -79,17 +79,20 @@ WSGI_APPLICATION = 'NovelWeb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#         #'ENGINE': 'django.db.backends.postgresql',
+#         #'NAME': 'novels',
+#         #'USER': 'ringo',
+#         #'PASSWORD': 'ringo',
+#         #'HOST': 'localhost',
+#         #'PORT': '5432',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        #'ENGINE': 'django.db.backends.postgresql',
-        #'NAME': 'novels',
-        #'USER': 'ringo',
-        #'PASSWORD': 'ringo',
-        #'HOST': 'localhost',
-        #'PORT': '5432',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 # Password validation
