aaa63d5e9af97466881deb9f63e6749980bfd3d4
phamt1552001 <phamt1552001@gmail.com>
2025-03-11 18:25:10 +0700

    Cập nhật database trong file setting.py

--- NovelWeb/settings.py
+++ NovelWeb/settings.py
@@ -15,9 +15,6 @@ django_heroku.settings(locals())
 import os
 from pathlib import Path
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -92,9 +89,11 @@ WSGI_APPLICATION = 'NovelWeb.wsgi.application'
 #     }
 # }
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
